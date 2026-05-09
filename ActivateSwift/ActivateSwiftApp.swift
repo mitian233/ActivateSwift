95c8bdc8b6f20e22a0119935e80f7e5482549e01
mitian233 <mitian233@yahoo.co.jp>
2026-05-09 21:37:24 +0800

    Initial Commit

---
+++ ActivateSwift/ActivateSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivateSwiftApp.swift
+//  ActivateSwift
+//
+//  Created by 原田蜜柑 on 2026/05/09.
+//
+
+import SwiftUI
+
+@main
+struct ActivateSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
